e03364a54e6466e6320f70083b45dbf7da1f67f3
clethrill <clethrill@gmail.com>
2015-05-21 17:54:58 +1000

    Initial Front-End Commit

---
+++ web/js/functions.js
@@ -0,0 +1,92 @@
+//on page load
+$(function() {
+	jQuery.fn.extend({
+		t1makeActive: function() {
+			$(".acc_nav").children('li').each(function() {
+				$(this).removeClass("active");
+			});
+			$(this).addClass("active");
+		}
+	});
+	jQuery.fn.extend({
+		t2makeActive: function() {
+			$(".nav_pane").children('ul').each(function() {
+				$(this).removeClass("active");
+			});
+			$(this).addClass("active");
+		}
+	});
+	jQuery.fn.extend({
+		t3makeActive: function() {
+			$(this).parent().children('li').each(function() {
+				$(this).removeClass("active");
+			});
+			$(this).addClass("active");
+		}
+	});
+	//tier 1 navigation
+	$("#t1_1").click(function() {
+		$("#t1_1").t1makeActive();
+		$("#t2_1").t2makeActive();
+		$("#res_1").t3makeActive();
+		$("#t1").text("Reservations");
+		$("#t2").text("Search for a flight");
+		$("#page_title").text("Search for flight");
+		$(".inner_frame").css("border-top-left-radius", "0px");
+		$(".outer_frame").css("border-top-left-radius", "0px");
+		$(".outer_frame").css("border-top-right-radius", "5px");
+	});
+	$("#t1_2").click(function() {
+		$("#t1_2").t1makeActive();
+		$("#t2_2").t2makeActive();
+		$("#pro_1").t3makeActive();
+		$("#t1").text("Profile");
+		$("#t2").text("Account Settings");
+		$("#page_title").text("Account Settings");
+		$(".inner_frame").css("border-top-left-radius", "0px");
+		$(".outer_frame").css("border-top-left-radius", "5px");
+		$(".outer_frame").css("border-top-right-radius", "5px");
+	});
+	$("#t1_3").click(function() {
+		$("#t1_3").t1makeActive();
+		$("#t2_3").t2makeActive();
+		$("#ser_1").t3makeActive();
+		$("#t1").text("Services");
+		$("#t2").text("Our Services");
+		$("#page_title").text("Our Services");
+		$(".inner_frame").css("border-top-left-radius", "0px");
+		$(".outer_frame").css("border-top-left-radius", "5px");
+		$(".outer_frame").css("border-top-right-radius", "5px");
+	});
+	$("#t1_4").click(function() {
+		$("#t1_4").t1makeActive();
+		$("#t2_4").t2makeActive();
+		$("#rep_1").t3makeActive();
+		$("#t1").text("Reports");
+		$("#t2").text("Travel Report");
+		$("#page_title").text("Travel Report");
+		$(".inner_frame").css("border-top-left-radius", "0px");
+		$(".outer_frame").css("border-top-left-radius", "5px");
+		$(".outer_frame").css("border-top-right-radius", "0px");
+	});
+	//tier 3 navigation
+	$(".nav_pane li").click(function() {
+		$(this).t3makeActive();
+		if ( $(this).is(":first-child") ) {
+			$(".inner_frame").css("border-top-left-radius", "0px");
+		} else {
+			$(".inner_frame").css("border-top-left-radius", "5px");
+		}
+		var el_text = $(this).text();
+		$("#t2").text(el_text);
+		$("#page_title").text(el_text);
+	});
+	//Search flight form
+	$("#return").click(function() {
+		if($(this).parent().find('input').is(':checked')) {
+			$("#return_date").removeAttr("disabled");
+		} else {
+			$("#return_date").attr("disabled", "disabled");
+		}
+	});
+});
